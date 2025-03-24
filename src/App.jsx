e489b06ab874b999e2becec63bd60527b82b8d17
Samuel Nunes <samuelsalomao883@gmail.com>
2025-03-23 21:13:46 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css';
 import HelloWorld from './components/HelloWorld.jsx';
 import SayMyName from './components/SayMyName.jsx';
 import Pessoa from './components/Pessoa.jsx';
+import Frase from './components/Frase.jsx';
 
 function App() {
   const nome = "Maria";
   return (
       <div>
+        <h1>Testando CSS</h1>
+        <Frase/>
+        <Frase/>
         <SayMyName nome="Samuel"/>
         <SayMyName nome="Pedro"/>
         <SayMyName nome={nome}/>
