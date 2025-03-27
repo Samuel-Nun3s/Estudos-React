e821be74560714d4119f2a10883df4e9aeacdce1
Samuel Nunes <samuelsalomao883@gmail.com>
2025-03-26 21:28:04 -0300

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import './App.css';
-import { useState } from "react";
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Contato from "./pages/Contato";
+import Empresa from "./pages/Empresa";
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
-  const [nome, setNome] = useState();
 
   return (
-      <div>
-        <h1>State Lift</h1>
-        <SeuNome setNome={setNome} />
-        <Saudacao nome={nome}/>
-      </div>
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path='/empresa' element={<Empresa/>}/>
+          <Route path='/contato' element={<Contato/>}/>
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
+    </div>
   )   
 }
 
-export default App
+export default App;
